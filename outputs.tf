1a9ae7bd66385a300d8344ea90f11b8f8e0f8708
tay2345-gif <TashuanLawrence7@gmail.com>
2025-05-01 12:48:07 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,11 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "kubeconfig" {
-  value = module.eks.kubeconfig
-  sensitive = true
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
